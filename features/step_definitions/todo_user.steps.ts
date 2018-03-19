0bef1926d96b9dfa29c50626aef5e382f41c673e
Jacek Góraj <jacek.goraj@gft.com>
2018-03-19 13:03:40 +0100

    added stage

--- features/step_definitions/todo_user.steps.ts
+++ features/step_definitions/todo_user.steps.ts
@@ -1,4 +1,5 @@
 import { Actor, BrowseTheWeb } from 'serenity-js/protractor';
+import { serenity } from 'serenity-js';
 import { AddATodoItem } from './../../spec/screenplay/tasks/add_a_todo_item';
 import { expect } from './../../spec/expect';
 import { listOf } from './../../spec/text';
@@ -6,26 +7,27 @@ import { protractor } from 'protractor';
 import { Start } from './../../spec/screenplay/tasks/start';
 import { TodoList } from './../../spec/screenplay/components/todo_list';
 
+let stage = serenity.callToStageFor({
+    actor: (name) => Actor.named(name).whoCan(BrowseTheWeb.using(protractor.browser))
+});
+
 export = function todoUserSteps() {
 
     this.setDefaultTimeout(30 * 1000);
 
-    let actor: Actor;
-
     this.Given(/^.*that (.*) has a todo list containing (.*)$/, function (name: string, items: string) {
-        actor = Actor.named(name).whoCan(BrowseTheWeb.using(protractor.browser));
-        return actor.attemptsTo(
+        return stage.theActorCalled(name).attemptsTo(
             Start.withATodoListContaining(listOf(items))
         );
     });
     
     this.When(/^s?he adds (.*?) to (?:his|her) list$/, function (itemName: string) {
-        return actor.attemptsTo(
+        return stage.theActorInTheSpotlight().attemptsTo(
             AddATodoItem.called(itemName)
         );
     });
     
     this.Then(/^.* todo list should contain (.*?)$/, function (items: string) {
-        return expect(actor.toSee(TodoList.Items_Displayed)).eventually.deep.equal(listOf(items));
+        return expect(stage.theActorInTheSpotlight().toSee(TodoList.Items_Displayed)).eventually.deep.equal(listOf(items));
     });
 };
